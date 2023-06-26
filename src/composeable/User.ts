b486df871eac69720fdd9bd27e45a935bb66b310
Gabriel Gajdoš <gajdosgabo@gmail.com>
2023-06-26 10:18:15 +0200

    User composeable

--- src/composeable/User.ts
+++ src/composeable/User.ts
@@ -4,6 +4,8 @@ import { reactive, readonly, computed } from "vue";
 
 const defaultState = {
   user: {},
+  isLoggedIn: false,
+  token: localStorage.getItem('token')
 };
 
 const state = reactive(defaultState);
@@ -13,10 +15,42 @@ const getters = {
 };
 
 const actions = {
+  fetchUser: async () => {
+    try {
+      await axios.get("/api/user")
+        .then(
+          response => {
+            state.user = response.data.data;
+            if (response.data.data) {
+              actions.updateIsLoggedIn(true);
+            }
+          }
+        )
+
+
+    } catch (e) { }
+  },
+
+  login: async (form) => {
+    axios.post("/api/sanctumLogin", form)
+      .then(
+        response => {
+          localStorage.setItem('token', response.data);
+          state.token = response.data
+          actions.fetchUser();
+        }
+      )
+
+  },
+
   getUser: async () => {
-    let response = await  axios.get("http://eventapi.local/api/user/");
+    let response = await axios.get("http://eventapi.local/api/user/");
     state.user = response.data.data;
   },
+
+  updateIsLoggedIn: (isLoggedIn) => {
+    state.isLoggedIn = isLoggedIn;
+  }
 };
 
 export default () => ({
