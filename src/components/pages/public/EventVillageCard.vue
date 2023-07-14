54f6f94e0fac1ae8c14d5f4f3ae4661e743dd3d7
Gabriel Gajdoš <gajdosgabo@gmail.com>
2023-07-14 17:02:16 +0200

    Aside query events villages

--- src/components/pages/public/EventVillageCard.vue
+++ src/components/pages/public/EventVillageCard.vue
@@ -24,19 +24,10 @@ export default defineComponent({
         <div class="flex space-x-1">
           <a href="#">
             <div class="flex hover:bg-gray-100 p-2 rounded-full h-9 w-9 items-center justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-5 h-5"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5"
-                />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-5 h-5">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5" />
               </svg>
             </div>
           </a>
@@ -46,12 +37,12 @@ export default defineComponent({
 
     <div class="p-4">
       <ul class="divide-y-2 divide-gray-200 divide-dashed">
-        <li v-for="village in villages" :key="village.id" class="flex justify-between">
+        <li v-for="(village, index) in villages" :key="village.id" class="flex justify-between">
           <router-link to="#">
             <!-- <i style="color: #3b32b3" class="fas fa-check"></i> -->
-            {{ village.fullname }}
+            {{ village[0].name }}
           </router-link>
-          <span>(N)</span>
+          <span>({{ village.length }})</span>
         </li>
         <li class="flex justify-between">
           <router-link to="#">
